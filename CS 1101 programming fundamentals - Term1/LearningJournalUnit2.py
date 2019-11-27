ffda7a7ae55c8ed4c6717d80479bb02da5fbf733
Abdelrahman Hamed <44338393+abdel24hour@users.noreply.github.com>
2019-11-28 01:10:06 +0200

    Create LearningJournalUnit2.py

---
+++ CS 1101 programming fundamentals - Term1/LearningJournalUnit2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# This is Part 1 for Learning Journal Unit 2 - UoPeople
+
+
+def print_volume(r):                                # Creating print_volume function with R as argument
+     pi = 3.141592653589793                         # Here's the value of pi
+     V = 4.0/3.0*pi*r**3                            # Here's how I calculated V (4.0/3.0*3.141592653589793* r * r * r)
+     print("The volume is",V)                       # Printing the output
+
+print_volume(3)                                     # When R is 3
+
+print_volume(10)                                    # When R is 10
+
+print_volume(50)                                     # When R is 50
+
+
+# Part 2 for Learning Journal Unit 2
+
+def print_myfunction():
+    print ("I learned what is python")
+    print ("I learned how to solve math with python")
+    print ("I learned what is the meaning of variable, argument, parameters")
+    print ("I googled for hours to try to do those assignments")
+    print ("Now I know how I can create function, print and call it!")
+    print ("I'm enjoying learning python")
+
+
+print_myfunction()
