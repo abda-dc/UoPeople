79a82624145eafb1d99d24bf6663c9fef0098276
Abdelrahman Hamed <44338393+abdel24hour@users.noreply.github.com>
2019-12-26 00:49:35 +0200

    Create Box.java

---
+++ CS 1102 Programming 1 - Term 2/Programming Assign. Unit 6/Box.java
@@ -0,0 +1,28 @@
+/*
+ * @author abdelrahman
+ */
+
+package main;
+
+public class Box {
+  
+  double width;
+  double height;
+  double depth;
+
+  // This is an empty constructor
+  public Box() {
+    ;
+  }
+
+  // This is a constructor with attributes
+  public Box(double w, double h, double d) {
+    width = w;
+    height = h;
+    depth = d;
+  }
+
+  public void getVolume() {
+    System.out.println("Volume is : " + width * height * depth);
+  }
+}
