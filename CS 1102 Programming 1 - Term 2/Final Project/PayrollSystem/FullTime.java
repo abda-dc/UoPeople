fc6385665985a7198b2c312addc7f77a1c8866cf
Abdelrahman Hamed <44338393+abdel24hour@users.noreply.github.com>
2020-01-12 19:19:43 +0200

    Create FullTime.java

---
+++ CS 1102 Programming 1 - Term 2/Final Project/PayrollSystem/FullTime.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrollsystem;
+
+/**
+ *
+ * @author abdelrahman
+ */
+public class FullTime extends Employee {
+
+     private double salary;         
+     private double overtime;
+
+     
+      public FullTime(int id, String name, double sa1, double hourAndHalf, Vehicle vehicle) 
+      
+      {
+    
+           super (id, name, vehicle);
+           this.overtime = hourAndHalf;
+           this.salary = sa1;
+    
+    }
+     
+     
+     public double calculatePay() {
+            
+          System.out.println("Full time employee.");
+          return (this.getSalary() + this.getOvertime());
+        
+        }    
+
+    /**
+     * @return the salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the overtime
+     */
+    public double getOvertime() {
+        return overtime;
+    }
+
+    /**
+     * @param overtime the overtime to set
+     */
+    public void setOvertime(double overtime) {
+        this.overtime = overtime;
+    }
+}
